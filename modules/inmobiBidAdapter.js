1cfe8f0ff6097b22ae98fa76691cddcbd6b44803
Basil Goldman <>
2025-04-08 11:17:22 +0200

    update rubicon, smilewanted + add inmobi adapters

--- modules/inmobiBidAdapter.js
+++ modules/inmobiBidAdapter.js
@@ -1,185 +1,347 @@
-import {VIDEO} from '../src/mediaTypes.js';
+import { deepAccess, deepSetValue, isFn } from '../src/utils.js';
+import { registerBidder } from '../src/adapters/bidderFactory.js';
+import { BANNER, NATIVE, VIDEO } from '../src/mediaTypes.js';
 import { ortbConverter } from '../libraries/ortbConverter/converter.js';
-import {registerBidder} from '../src/adapters/bidderFactory.js';
-import {deepAccess, deepSetValue} from '../src/utils.js';
+import { ortb25Translator } from '../libraries/ortb2.5Translator/translator.js';
+import { tryAppendQueryString } from '../libraries/urlUtils/urlUtils.js';
 
+/**
+ * @typedef {import('../src/adapters/bidderFactory.js').BidRequest} BidRequest
+ * @typedef {import('../src/adapters/bidderFactory.js').Bid} Bid
+ * @typedef {import('../src/adapters/bidderFactory.js').ServerRequest} ServerRequest
+ * @typedef {import('../src/adapters/bidderFactory.js').BidderSpec} BidderSpec
+ * @typedef {import('../src/adapters/bidderFactory.js').TimedOutBid} TimedOutBid
+ */
+
+const GVLID = 333;
+export const ADAPTER_VERSION = 1.0;
 const BIDDER_CODE = 'inmobi';
-const ENDPOINT_URL = 'https://api.w.inmobi.com/showad/openrtb/bidder/prebid';
-const DEFAULT_CURRENCY = 'USD';
-const NET_REVENUE = false;
+const BID_ENDPOINT = 'https://api.w.inmobi.com/openrtb/bidder/prebidjs';
+export const EVENT_ENDPOINT = 'https://sync.inmobi.com';
+export const SYNC_ENDPOINT = 'https://sync.inmobi.com/prebidjs?';
+const TRANSLATOR = ortb25Translator();
+const CURRENCY = 'USD';
+const POST_METHOD = 'POST';
+const PLAIN_CONTENT_TYPE = 'text/plain';
 
-const converter = ortbConverter({
+/**
+ * Defines the core oRTB converter inherited from converter library and all customization functions.
+ */
+const CONVERTER = ortbConverter({
   context: {
-    netRevenue: NET_REVENUE, // or false if your adapter should set bidResponse.netRevenue = false
-    ttl: 300 // default bidResponse.ttl (when not specified in ORTB response.seatbid[].bid[].exp)
-  }
+    netRevenue: true,
+    ttl: 3600,
+    currency: CURRENCY
+  },
+  imp,
+  request,
+  bidResponse,
+  response
 });
 
+/**
+ * Builds an impression object for oRTB 2.5 requests based on the bid request.
+ *
+ * @param {function} buildImp - Function to build the imp object.
+ * @param {Object} bidRequest - The request containing bid details.
+ * @param {Object} context - Context for the impression.
+ * @returns {Object} The constructed impression object.
+ */
+function imp(buildImp, bidRequest, context) {
+  let imp = buildImp(bidRequest, context);
+  const params = bidRequest.params;
+
+  imp.tagid = bidRequest.adUnitCode;
+  let floorInfo = {};
+
+  if (isFn(bidRequest.getFloor)) {
+    floorInfo = bidRequest.getFloor({
+      currency: CURRENCY,
+      size: '*',
+      mediaType: '*'
+    });
+  }
+
+  // if floor price module is not set reading from bidRequest.params
+  if (!imp.bidfloor && bidRequest.params.bidfloor) {
+    imp.bidfloor = bidRequest.params.bidfloor;
+    imp.bidfloorcur = CURRENCY;
+  }
+
+  deepSetValue(imp, 'ext', {
+    ...imp.ext,
+    params: bidRequest.params,
+    bidder: {
+      plc: params?.plc,
+    },
+    moduleFloors: floorInfo
+  });
+  imp.secure = Number(window.location.protocol === 'https:');
+
+  return imp;
+}
+
+/**
+ * Constructs the oRTB 2.5 request object.
+ *
+ * @param {function} buildRequest - Function to build the request.
+ * @param {Array} imps - Array of impression objects.
+ * @param {Object} bidderRequest - Object containing bidder request information.
+ * @param {Object} context - Additional context.
+ * @returns {Object} The complete oRTB request object.
+ */
+function request(buildRequest, imps, bidderRequest, context) {
+  let request = buildRequest(imps, bidderRequest, context);
+
+  deepSetValue(request, 'ext.prebid.channel.name', 'pbjs_InMobi');
+  deepSetValue(request, 'ext.prebid.channel.pbjsversion', '$prebid.version$');
+  deepSetValue(request, 'ext.prebid.channel.adapterversion', ADAPTER_VERSION);
+
+  request = TRANSLATOR(request);
+  return request;
+}
+
+/**
+ * Transforms an oRTB 2.5 bid into a bid response format for Prebid.js.
+ *
+ * @param {function} buildBidResponse - Function to build a bid response.
+ * @param {Object} bid - The bid to be transformed.
+ * @param {Object} context - Context for the bid.
+ * @returns {Object} Formatted bid response.
+ */
+function bidResponse(buildBidResponse, bid, context) {
+  context.mtype = deepAccess(bid, 'mtype');
+  if (context.mtype === 4) {
+    const admJson = JSON.parse(bid.adm);
+    bid.adm = JSON.stringify(admJson.native);
+  }
+  let bidResponse = buildBidResponse(bid, context);
+
+  if (typeof deepAccess(bid, 'ext') !== 'undefined') {
+    deepSetValue(bidResponse, 'meta', {
+      ...bidResponse.meta,
+      ...bid.ext,
+    });
+  }
+
+  return bidResponse;
+}
+
+/**
+ * Converts the oRTB 2.5 bid response into the format required by Prebid.js.
+ *
+ * @param {function} buildResponse - Function to build the response.
+ * @param {Array} bidResponses - List of bid responses.
+ * @param {Object} ortbResponse - Original oRTB response data.
+ * @param {Object} context - Additional context.
+ * @returns {Object} Prebid.js compatible bid response.
+ */
+function response(buildResponse, bidResponses, ortbResponse, context) {
+  let response = buildResponse(bidResponses, ortbResponse, context);
+
+  return response;
+}
+
+/** @type {BidderSpec} */
 export const spec = {
   code: BIDDER_CODE,
-  gvlid: 333,
-  supportedMediaTypes: [VIDEO],
-  isBidRequestValid: (bid) => {
-    return !!(bid.params && bid.params.plc);
-  },
-  buildRequests: (validBidRequests, bidderRequest) => {
-    const data = converter.toORTB({validBidRequests, bidderRequest});
-    deepSetValue(data.imp[0], 'ext.bidder.plc', validBidRequests[0].params.plc);
-    return {
-      method: 'POST',
-      url: ENDPOINT_URL,
-      data,
-      bidRequest: validBidRequests[0]
-    };
-    /*
-    return validBidRequests.map(bidRequest => {
+  gvlid: GVLID,
+  supportedMediaTypes: [BANNER, VIDEO, NATIVE],
 
-      let refererInfo;
-      if (bidderRequest && bidderRequest.refererInfo) {
-        refererInfo = bidderRequest.refererInfo;
-      }
+  /**
+   * Determines user sync options based on consent and supported sync types.
+   *
+   * @param {Object} syncOptions - Options for user syncing (iframe, pixel).
+   * @param {Array} responses - List of bid responses.
+   * @param {Object} gdprConsent - GDPR consent details.
+   * @param {Object} uspConsent - CCPA consent details.
+   * @param {Object} gppConsent - GPP consent details.
+   * @returns {Array} List of user sync URLs.
+   */
+  getUserSyncs: (syncOptions, responses, gdprConsent, uspConsent, gppConsent) => {
+    const urls = [];
+    if (!syncOptions.iframeEnabled && !syncOptions.pixelEnabled) {
+      return urls;
+    }
+    const pixelType = syncOptions.iframeEnabled ? 'iframe' : 'image';
 
-      let payload = {
-        id: bidRequest.transactionId,
-        at: 1,
-        cur: [DEFAULT_CURRENCY],
-        imp: [{
-          id: bidRequest.adUnitCode,
-          secure: 1,
-          video: deepAccess(bidRequest, 'mediaTypes.video') || {},
-          ext: {
-            bidder: {
-              plc: bidRequest.params.plc
-            }
-          }
-        }],
-        site: {
-          page: refererInfo.page,
-          ref: refererInfo.ref,
-          domain: refererInfo.domain,
-          publisher: {}
-        },
-        device: {
-          ua: navigator.userAgent,
-          js: 1,
-          dnt: (navigator.doNotTrack == 'yes' || navigator.doNotTrack == '1' || navigator.msDoNotTrack == '1') ? 1 : 0,
-          h: screen.height,
-          w: screen.width,
-          language: navigator.language
-        },
-        source: {
-          tid: bidRequest.transactionId
-        },
-        tmax: bidderRequest.timeout,
-        user: {},
-        ext: {}
-      };
-
-      // Size
-      let size = [];
-      if (Array.isArray(deepAccess(bidRequest, 'mediaTypes.video.playerSize')) && bidRequest.mediaTypes.video.playerSize.length === 1) {
-        size = bidRequest.mediaTypes.video.playerSize[0];
-      } else if (Array.isArray(bidRequest.sizes) && bidRequest.sizes.length > 0 && Array.isArray(bidRequest.sizes[0]) && bidRequest.sizes[0].length > 1) {
-        size = bidRequest.sizes[0];
-      }
-      if (size.length > 0) {
-        payload.imp[0].video.w = size[0];
-        payload.imp[0].video.h = size[1];
-      }
-
-      // Floor
-      let bidFloor = parseFloat(deepAccess(bidRequest, 'params.bidfloor'));
-      if (!isNaN(bidFloor)) {
-        payload.imp[0].bidfloor = bidFloor;
-      }
-
-      // Merge the device from config.getConfig('device')
-      if (typeof config.getConfig('device') === 'object') {
-        payload.device = Object.assign(payload.device, config.getConfig('device'));
-      }
-
-      // Supply Chain (Schain)
-      if (bidRequest.schain) {
-        deepSetValue(payload, 'source.ext.schain', bidRequest.schain);
-      }
-
-      // GDPR & Consent (EU)
-      if (bidderRequest && bidderRequest.gdprConsent) {
-        deepSetValue(payload, 'user.ext.consent', bidderRequest.gdprConsent.consentString);
-        deepSetValue(payload, 'regs.ext.gdpr', (bidderRequest.gdprConsent.gdprApplies ? 1 : 0));
-      }
-
-      // CCPA (US Privacy)
-      if (bidderRequest && bidderRequest.uspConsent) {
-        deepSetValue(payload, 'regs.ext.us_privacy', bidderRequest.uspConsent);
-      }
-
-      // User Ids
-      const eids = deepAccess(bidderRequest, 'bids.0.userIdAsEids');
-      if (eids && eids.length) {
-        deepSetValue(payload, 'user.ext.eids', eids);
-      }
-
-      // COPPA
-      if (config.getConfig('coppa') === true) {
-        deepSetValue(payload, 'regs.coppa', 1);
-      }
-
-      return {
-        method: 'POST',
-        url: ENDPOINT_URL,
-        data: JSON.stringify(payload),
-        bidRequest
-      };
-    });
-     */
-  },
-  interpretResponse: function(serverResponse, { bidRequest }) {
-    serverResponse = serverResponse.body;
-
-    const bids = [];
-    // Check overall response
-    if (!serverResponse || typeof serverResponse !== 'object') {
-      return bids;
+    let query = '';
+    if (gdprConsent) {
+      query = tryAppendQueryString(query, 'gdpr', (gdprConsent.gdprApplies ? 1 : 0));
+    }
+    if (gdprConsent && typeof gdprConsent.consentString === 'string') {
+      query = tryAppendQueryString(query, 'gdpr_consent', gdprConsent.consentString);
+    }
+    if (uspConsent) {
+      query = tryAppendQueryString(query, 'us_privacy', uspConsent);
+    }
+    if (gppConsent?.gppString && gppConsent?.applicableSections?.length) {
+      query = tryAppendQueryString(query, 'gpp', gppConsent.gppString);
+      query = tryAppendQueryString(query, 'gpp_sid', gppConsent?.applicableSections?.join(','));
+    }
+    if (query.slice(-1) === '&') {
+      query = query.slice(0, -1);
     }
 
-    // Video response
-    if (serverResponse.seatbid) {
-      serverResponse.seatbid.forEach(seatbid => {
-        (seatbid.bid || []).forEach(bid => {
-          let bidObject = {
-            requestId: bidRequest.bidId,
-            cpm: bid.price || 0,
-            currency: serverResponse.cur || DEFAULT_CURRENCY,
-            creativeId: bid.crid || bid.id,
-            bidderCode: BIDDER_CODE, // seatbid.seat,
-            ttl: 300,
-            ad: bid.adm,
-            netRevenue: NET_REVENUE,
-            width: bid.w || deepAccess(bidRequest, 'mediaTypes.video.w') || deepAccess(bidRequest, 'params.video.playerWidth'),
-            height: bid.h || deepAccess(bidRequest, 'mediaTypes.video.h') || deepAccess(bidRequest, 'params.video.playerHeight'),
-            mediaType: VIDEO
-          };
+    if (pixelType === 'iframe' || (!responses || responses.length === 0)) {
+      return [{
+        type: pixelType,
+        url: SYNC_ENDPOINT + query
+      }];
+    } else {
+      responses.forEach(resp => {
+        const userSyncs = deepAccess(resp, 'body.ext.prebidjs.urls');
+        if (!userSyncs) {
+          return;
+        }
 
-          if (bid.adm) {
-            bidObject.vastXml = bid.adm;
+        userSyncs.forEach(us => {
+          let url = us.url;
+          if (query) {
+            url = url + (url.indexOf('?') === -1 ? '?' : '&') + query;
           }
 
-          if (bid.id) {
-            bidObject.seatBidId = bid.id;
-          }
-
-          if (bid.adomain) {
-            deepSetValue(bidObject, 'meta.advertiserDomains', Array.isArray(bid.adomain) ? bid.adomain : [bid.adomain]);
-          }
-
-          bids.push(bidObject);
+          urls.push({
+            type: pixelType,
+            url: url
+          });
         });
       });
-
-      return bids;
+      return urls;
     }
+  },
+
+  /**
+   * Validates if a bid request contains the required parameters for InMobi.
+   *
+   * @param {Object} bid - Bid request to validate.
+   * @returns {boolean} True if the bid request is valid, otherwise false.
+   */
+  isBidRequestValid: (bid) => {
+    if (!(bid && bid.params && bid.params.plc)) {
+      return false;
+    }
+
+    return true;
+  },
+
+  /**
+   * Builds the server request from bid requests for InMobi.
+   *
+   * @param {BidRequest[]} bidRequests - Array of bid requests.
+   * @param {Object} bidderRequest - Additional request details.
+   * @returns {ServerRequest} The server request for bidding.
+   */
+  buildRequests: (bidRequests, bidderRequest) => {
+    const data = CONVERTER.toORTB({ bidderRequest, bidRequests });
+
+    if (data) {
+      const requestPayload = {
+        method: POST_METHOD,
+        url: BID_ENDPOINT,
+        data: data,
+        options: {
+          contentType: PLAIN_CONTENT_TYPE,
+          crossOrigin: true,
+          withCredentials: true
+        }
+      };
+      return requestPayload;
+    }
+  },
+
+  /**
+   * Interprets the server response and formats it into bids.
+   *
+   * @param {Object} response - Response from the server.
+   * @param {ServerRequest} request - Original bid request.
+   * @returns {Bid[]} Parsed bids or configurations.
+   */
+  interpretResponse: (response, request) => {
+    if (typeof response?.body == 'undefined') {
+      return [];
+    }
+
+    const interpretedResponse = CONVERTER.fromORTB({ response: response.body, request: request.data });
+    const bids = interpretedResponse.bids || [];
+
+    return bids;
+  },
+
+  /**
+   * Callback to report timeout event.
+   *
+   * @param {TimedOutBid[]} timeoutData - Array of timeout details.
+   */
+  onTimeout: (timeoutData) => {
+    report('onTimeout', timeoutData);
+  },
+
+  /**
+   * Callback to report targeting event.
+   *
+   * @param {Bid} bid - The bid object
+   */
+  onSetTargeting: (bid) => {
+    report('onSetTargeting', bid?.meta);
+  },
+
+  /**
+   * Callback to report successful ad render event.
+   *
+   * @param {Bid} bid - The bid that successfully rendered.
+   */
+  onAdRenderSucceeded: (bid) => {
+    report('onAdRenderSucceeded', bid?.meta);
+  },
+
+  /**
+   * Callback to report bidder error event.
+   *
+   * @param {Object} errorData - Details about the error.
+   */
+  onBidderError: (errorData) => {
+    report('onBidderError', errorData);
+  },
+
+  /**
+   * Callback to report bid won event.
+   *
+   * @param {Bid} bid - The bid that won the auction.
+   */
+  onBidWon: (bid) => {
+    report('onBidWon', bid?.meta);
   }
+
 };
+
+function isReportingAllowed(loggingPercentage) {
+  return loggingPercentage != 0;
+}
+
+function report(type, data) {
+  if (!data) {
+    return;
+  }
+  if (['onBidWon', 'onAdRenderSucceeded', 'onSetTargeting'].includes(type) && !isReportingAllowed(data.loggingPercentage)) {
+    return;
+  }
+  const payload = JSON.stringify({
+    domain: location.hostname,
+    eventPayload: data
+  });
+
+  fetch(`${EVENT_ENDPOINT}/report/${type}`, {
+    body: payload,
+    keepalive: true,
+    credentials: 'include',
+    method: POST_METHOD,
+    headers: {
+      'Content-Type': PLAIN_CONTENT_TYPE
+    }
+  }).catch((_e) => {
+    // do nothing; ignore errors
+  });
+}
 
 registerBidder(spec);
