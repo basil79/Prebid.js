c8cb223eba5404096c8ca8ea6aa5619356543450
Basil Goldman <>
2025-09-04 10:16:31 +0200

    comment ozon test bids when debug is enabled

--- modules/ozoneBidAdapter.js
+++ modules/ozoneBidAdapter.js
@@ -318,7 +318,7 @@ export const spec = {
       'page': getRefererInfo().page,
       'id': htmlParams.siteId
     };
-    ozoneRequest.test = config.getConfig('debug') ? 1 : 0;
+    //ozoneRequest.test = config.getConfig('debug') ? 1 : 0;
     if (bidderRequest && bidderRequest.gdprConsent) {
       logInfo('ADDING GDPR');
       let apiVersion = deepAccess(bidderRequest, 'gdprConsent.apiVersion', 1);
