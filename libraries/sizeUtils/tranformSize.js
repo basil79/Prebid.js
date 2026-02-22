09f8d0d5d3851781eeff8b11a5bd1f1bf6207054
Basil Goldman <>
2026-02-22 12:47:24 +0100

    fixes + adapters

--- libraries/sizeUtils/tranformSize.js
+++ libraries/sizeUtils/tranformSize.js
@@ -6,20 +6,20 @@ import * as utils from '../../src/utils.js';
  * @return {Object}
  */
 export function transformSizes(requestSizes) {
-  let sizes = [];
+  const sizes = [];
   let sizeObj = {};
 
   if (
     utils.isArray(requestSizes) &&
-      requestSizes.length === 2 &&
-      !utils.isArray(requestSizes[0])
+    requestSizes.length === 2 &&
+    !utils.isArray(requestSizes[0])
   ) {
     sizeObj.width = parseInt(requestSizes[0], 10);
     sizeObj.height = parseInt(requestSizes[1], 10);
     sizes.push(sizeObj);
   } else if (typeof requestSizes === 'object') {
     for (let i = 0; i < requestSizes.length; i++) {
-      let size = requestSizes[i];
+      const size = requestSizes[i];
       sizeObj = {};
       sizeObj.width = parseInt(size[0], 10);
       sizeObj.height = parseInt(size[1], 10);
